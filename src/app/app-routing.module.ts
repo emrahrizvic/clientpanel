b116553515c4654f032ff47ae1eb18e5e69758cb
Emrah Rizvić <emrah_rizvic@hotmail.com>
2019-03-28 00:49:13 +0100

    Added edit and delete client functionaliti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
 
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'add-client', component: AddClientComponent },
+  { path: 'client/:clientId', component: ClientDetailsComponent },
+  { path: 'edit-client/:clientId', component: EditClientComponent },
 ];
 
 @NgModule({
--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Client } from 'src/app/models/Client';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.scss']
 })
 export class ClientDetailsComponent implements OnInit {
+  clientId: string;
+  client: Client;
+  hasBalance = false;
+  showBalanceUpdateInput = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessagesService: FlashMessagesService,
+  ) { }
 
   ngOnInit() {
+    // Get ID
+    // tslint:disable-next-line:no-string-literal
+    this.clientId = this.route.snapshot.params['clientId'];
+
+    // Get client
+    this.clientService.getClient(this.clientId).subscribe(client => {
+      if (client.balance > 0) {
+        this.hasBalance = true;
+      }
+      this.client = client;
+    });
+  }
+
+  updateBalance(clientId: string) {
+    // Update client
+    this.clientService.updateClient(clientId, this.client);
+    this.flashMessagesService.show('Balance Updated.', {cssClass: 'flashfade alert_message alert-success', timeout: 5000});
+    this.router.navigate(['/client/' + clientId]);
+  }
+
+  onDeleteClick() {
+    if (confirm('Are you sure to delete?')) {
+      this.clientService.deleteClient(this.clientId);
+    }
+    this.flashMessagesService.show('Client Deleted.', {cssClass: 'flashfade alert_message alert-danger', timeout: 5000});
+    this.router.navigate(['/']);
   }
 
 }
